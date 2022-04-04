82bedd7c24eea1de05dff33b59298146ab931ff2
venkatasujatha <valluru.1sujatha@gmail.com>
2022-04-04 19:02:51 +0530

    departmentsuji

---
+++ DepartmentSuji/src/main/java/com/tectoro/dao/serviceInterface.java
@@ -0,0 +1,16 @@
+package com.tectoro.dao;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.tectoro.DeptEntity.DeptModel;
+
+public interface serviceInterface 
+{
+	public DeptModel save(DeptModel model);
+	public List<DeptModel> getDeptModel(DeptModel model);
+	public DeptModel update(@PathVariable int id);
+	public void delete(@PathVariable int id);
+	public String palindrome(String name);
+}
