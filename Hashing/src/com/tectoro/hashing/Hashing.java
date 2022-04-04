cf7cee53dd72dff979c2697a7ea51fde0d06aed1
venkatasujatha <valluru.1sujatha@gmail.com>
2022-04-04 13:06:22 +0530

    hashing

---
+++ Hashing/src/com/tectoro/hashing/Hashing.java
@@ -0,0 +1,21 @@
+package com.tectoro.hashing;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Hashing 
+{
+	public static void main(String[] args)
+	{
+		//HashSet hs=new HashSet();
+		LinkedHashSet hs=new LinkedHashSet();
+		hs.add(120);
+		hs.add(60);
+		hs.add(80);
+		hs.add(70);
+		hs.add(90);
+		hs.add(20);
+		System.out.println(hs);
+
+	}
+}
---
+++ Hashing/src/com/tectoro/hashing/hashset.java
@@ -0,0 +1,10 @@
+package com.tectoro.hashing;
+
+import java.util.HashSet;
+
+public class hashset 
+{
+	HashSet hs=new HashSet();
+	hs.add(120);
+	hs.add(60);
+}
